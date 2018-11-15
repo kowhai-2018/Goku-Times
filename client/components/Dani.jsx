477b9182eefa6a90542d64f87affc591cfa3b4d0
danielle-baek <schgana@hotmail.com>
2018-11-16 11:09:08 +1300

    added horoscope

--- client/components/Dani.jsx
+++ client/components/Dani.jsx
@@ -3,27 +3,37 @@ import request from 'superagent'
 
  export default class Dani extends React.Component {
   state = {
-    key: 'property'
+    json: {}
   }
   componentDidMount(){
-    // this.callfunction
+    this.getHoroscope
   }
 
-  callfunction(){
+  getHoroscope(){
+    const URL = 'https://aztro.sameerkumar.website/?sign=aries&day=today';
+       
     request
-    .get(this.state.network.urlIP)
+    .get(this.state.url)
     .then( res => {
-      this.setState({
-        network: {
-          ip: res.body.ip,
-          urlLocation: 'https://geo.ipify.org/api/v1?apiKey='+ this.state.network.apiK + '&ipAddress=' + res.body.ip
-        }
-      })
+      res.json()
+    })
+    .then(json => {
+      console.log(json)
+      this.setState({json}) 
     })
   }
     render() {
         return (
-          <h1>this is Dani's component</h1>
+          <React.Fragment>
+          Current Date: {this.state.json.current_date} <br />
+          Compatibility: {this.state.json.compatibility} <br />
+          Lucky Number: {this.state.json.lucky_number} <br />
+          Lucky Time: {this.state.json.lucky_time} <br />
+          Color: {this.state.json.color} <br />
+          Date Range: {this.state.json.date_range} <br />
+          Mood: {this.state.json.mood} <br />
+          Description: {this.state.json.description} <br />
+      </React.Fragment>
         )}
 }
 

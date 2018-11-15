42541cfca2d0d0760278e30ed4a2679c0dc95a76
Chris <chrislaupama@icloud.com>
2018-11-16 10:42:14 +1300

    got the geek jokes API working and in view

--- client/components/Chris.jsx
+++ client/components/Chris.jsx
@@ -1,29 +1,27 @@
 import React from 'react'
 import request from 'superagent'
 
- export default class Chris extends React.Component {
+export default class App extends React.Component {
   state = {
-    key: 'property'
-  }
-  componentDidMount(){
-    // this.callfunction
+    geekJokes: ''
   }
 
-  callfunction(){
+  componentDidMount() {
     request
-    .get(this.state.network.urlIP)
-    .then( res => {
-      this.setState({
-        network: {
-          ip: res.body.ip,
-          urlLocation: 'https://geo.ipify.org/api/v1?apiKey='+ this.state.network.apiK + '&ipAddress=' + res.body.ip
-        }
+      .get('https://geek-jokes.sameerkumar.website/api')
+      .then(res => {
+        this.setState({ geekJokes: res.body })
       })
-    })
+      .catch(err => {
+        this.setState({ message: err.message })
+      })
   }
-    render() {
-        return (
-          <h1>this is Chris's component</h1>
-        )}
-}
 
+  render() {
+    return (
+      <div>
+        <p>{this.state.geekJokes}</p>
+      </div>
+    )
+  }
+}
